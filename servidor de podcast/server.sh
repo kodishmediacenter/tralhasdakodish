98011d1612d703ae3803e5d27df6122c37bb2004
kodishmediacenter <fabiolmglgeo@gmail.com>
2021-09-15 22:43:33 -0300

    Create server.sh

---
+++ servidor de podcast/server.sh
@@ -0,0 +1,14 @@
+
+mkdir Bakuhatsucast
+wget https://raw.githubusercontent.com/kodishmediacenter/tralhasdakodish/master/servidor%20de%20podcast/BakuhatsuCast/nerdcast.py
+
+mkdir tokucast
+wget https://raw.githubusercontent.com/kodishmediacenter/tralhasdakodish/master/servidor%20de%20podcast/tokucast/nerdcast.py
+
+mkdir HenshinRio
+wget https://raw.githubusercontent.com/kodishmediacenter/tralhasdakodish/master/servidor%20de%20podcast/Henshin%20Rio/nerdcast.py
+
+
+
+
+
