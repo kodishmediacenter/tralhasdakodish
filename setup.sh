0f45680a475c9143d2e53dc9c12b03cddd71d41a
kodishmediacenter <fabiolmglgeo@gmail.com>
2022-01-25 15:55:58 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,14 @@ sudo apt install openbox obconf
 sudo apt install feh
 sudo apt install gdm3
 sudo apt install xint
-sudo cd ~/.config
+sudo cd /home/kodish/.config
 sudo mkdir openbox
 sudo apt install lilyterm 
 sudo apt install thunar
 sudo apt install firefox
+sudo apt install tint2
+git clone https://github.com/terroo/tint2-themes
+cd tint2-themes
+mv * ~/.config/tint2/
+tint2 -c  ~/.config/tint2/zug/zug.tint2rc
+
