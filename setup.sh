b21dfb2219e07bf72396cbd23c864296ce647ae6
kodishmediacenter <fabiolmglgeo@gmail.com>
2022-01-24 23:39:11 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@ sudo apt install openbox obconf
 sudo apt install feh
 sudo apt install gdm3
 sudo apt install xint
-cd ~/.config
-mkdir openbox
+sudo cd ~/.config
+sudo mkdir openbox
+sudo apt install lilyterm 
+sudo apt install thunar
