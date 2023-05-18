45ea4b2f6da932152439747ac579d21252996446
kodishmediacenter <fabiolmglgeo@gmail.com>
2023-05-18 19:25:25 -0300

    Create pyradio.sh

---
+++ pyradio.sh
@@ -0,0 +1,13 @@
+sudo apt-get install \
+    python3-full \
+    python3-pip \
+    python3-rich \
+    python3-requests \
+    python3-dnspython \
+    python3-psutil \
+    python3-netifaces \
+    python3-dateutil
+    
+sudo apt-get install vlc unzip 
+wget https://raw.githubusercontent.com/coderholic/pyradio/master/pyradio/install.py
+python3 install .py
